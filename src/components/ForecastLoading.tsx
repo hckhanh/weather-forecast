097b4efcc50619868c8ea4729e85c0ba421fd779
Khánh Hoàng <hoangchaukhanh93@gmail.com>
2021-02-03 21:52:13 +0700

    fix warning div inside p

--- src/components/ForecastLoading.tsx
+++ src/components/ForecastLoading.tsx
@@ -1,11 +1,16 @@
 import { Loading, Text } from "@geist-ui/react";
 import { ReactElement } from "react";
+import styled from "styled-components";
+
+const LoadingText = styled(Text)`
+  font-weight: 400;
+`;
 
 function ForecastLoading(): ReactElement {
   return (
-    <Text size={16}>
+    <LoadingText size={16} span>
       <Loading>Loading</Loading>
-    </Text>
+    </LoadingText>
   );
 }
 
